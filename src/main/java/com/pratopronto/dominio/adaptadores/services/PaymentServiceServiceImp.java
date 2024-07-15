fcae7c0a48d8828be17a4bc256a32ad6e7b4d024
matralves <malves@sauter.digital>
2024-07-15 18:59:54 -0300

    teste

--- src/main/java/com/pratopronto/dominio/adaptadores/services/PaymentServiceServiceImp.java
+++ src/main/java/com/pratopronto/dominio/adaptadores/services/PaymentServiceServiceImp.java
@@ -7,11 +7,13 @@ import com.pratopronto.dominio.dtos.order.UpdateOrderDTO;
 import com.pratopronto.dominio.enums.StatusEnum;
 import com.pratopronto.dominio.portas.interfaces.PaymentServicePort;
 import com.pratopronto.dominio.portas.repositories.OrderRepositoryPort;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.messaging.support.MessageBuilder;
 
+@Slf4j
 public class PaymentServiceServiceImp implements PaymentServicePort {
 
     private final QueueMessagingTemplate queueMessagingTemplate;
@@ -20,7 +22,6 @@ public class PaymentServiceServiceImp implements PaymentServicePort {
 
     @Value("${cloud.aws.end-point.uri}")
     private String endpoint;
-
     public PaymentServiceServiceImp(QueueMessagingTemplate queueMessagingTemplate, OrderRepositoryPort orderRepositoryPort, ObjectMapper objectMapper) {
         this.queueMessagingTemplate = queueMessagingTemplate;
         this.orderRepositoryPort = orderRepositoryPort;
@@ -30,7 +31,9 @@ public class PaymentServiceServiceImp implements PaymentServicePort {
     @Override
     public void sendPayment(Order order) {
         try {
-            queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload(objectMapper.writeValueAsString(order)).build());
+            String message = objectMapper.writeValueAsString(order);
+            log.info("publicando mensagem na fila de pagamentos: {}", message);
+            queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload(message).build());
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
@@ -40,6 +43,7 @@ public class PaymentServiceServiceImp implements PaymentServicePort {
     public void loadMessageFromSQS(String message) {
         Order order;
         try {
+            log.info("recebendo mensagem da fila de status de pedido: {}", message);
             order = objectMapper.readValue(message, Order.class);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
